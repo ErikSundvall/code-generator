5d31557210cee37656c0c1ccefb243c524404de0
Erik Sundvall <erik@sundvall.org>
2025-11-05 10:47:19 +0000

    refactor: Clean up BmmSingleProperty class by removing unnecessary comments and improving type handling in fromArray method

--- src/Model/Bmm/BmmSingleProperty.php
+++ src/Model/Bmm/BmmSingleProperty.php
@@ -1,16 +1,13 @@
 <?php
+// Paste this code into src/Model/Bmm/BmmSingleProperty.php
 
 namespace OpenEHR\Tools\CodeGen\Model\Bmm;
 
 use JsonSerializable;
 use OpenEHR\Tools\CodeGen\Model\YamlSerializable;
 
-/**
- * Class representing a BMM single property
- */
 readonly class BmmSingleProperty extends AbstractBmmProperty implements JsonSerializable, YamlSerializable
 {
-
     public function __construct(
         string $name,
         public BmmSimpleType|BmmContainerType|BmmGenericType $typeDef,
@@ -19,9 +16,6 @@ readonly class BmmSingleProperty extends AbstractBmmProperty implements JsonSeri
         parent::__construct($name);
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function jsonSerialize(): array
     {
         return array_filter([
@@ -30,9 +24,6 @@ readonly class BmmSingleProperty extends AbstractBmmProperty implements JsonSeri
         ]);
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function yamlSerialize(): array
     {
         return array_filter([
@@ -41,15 +32,34 @@ readonly class BmmSingleProperty extends AbstractBmmProperty implements JsonSeri
         ]);
     }
 
-    /**
-     * @param array<string, mixed> $data
-     * @return self
-     */
     public static function fromArray(array $data): self
     {
+        $typeDefData = null;
+
+        // Case 1: The modern 'type_def' object exists.
+        if (isset($data['type_def']) && is_array($data['type_def'])) {
+            $typeDefData = $data['type_def'];
+        }
+        // Case 2: The legacy 'type' string exists.
+        elseif (isset($data['type'])) {
+            $typeDefData = [
+                '_type' => 'BMM_SIMPLE_TYPE',
+                'type' => $data['type'],
+            ];
+        }
+        // Case 3: Neither exists, so we must default to 'ANY' to prevent a crash.
+        else {
+            $typeDefData = [
+                '_type' => 'BMM_SIMPLE_TYPE',
+                'type' => 'ANY',
+            ];
+            // also log a warning here
+            error_log("BMM schema '{$data['name']}' probably has an invalid type definition (neither type_def nor type found). Defaulting to 'ANY'.");
+        }
+
         return new self(
             name: $data['name'],
-            typeDef: AbstractBmmType::fromArray($data['type_def']),
+            typeDef: AbstractBmmType::fromArray($typeDefData)
         );
     }
 }
